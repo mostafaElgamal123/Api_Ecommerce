6d3f0ae449efa8b06fa823d1a79158f188d381f7
mostafa <mostafa.mohamed.azaz1234@gmail.com>
2022-10-10 13:38:50 +0200

    first commit

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,7 +13,4 @@ class Category extends Model
         'name',
         'description'
     ];
-    public function products(){
-        return $this->hasMany(Product::class,'category_id');
-    }
 }
